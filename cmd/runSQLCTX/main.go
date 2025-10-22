d317437e8d313ba3f62e10164a5ef5523e6c1fa2
Jonilson Sousa <jonilsonds9@gmail.com>
2025-10-22 18:03:35 -0300

    Iniciando com trasações

---
+++ cmd/runSQLCTX/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jonilsonds9/goexpert-modulo-15-sqlc/internal/db"
+)
+
+type CourseDB struct {
+	dbConn *sql.DB
+	*db.Queries
+}
+
+func NewCourseDB(dbConn *sql.DB) *CourseDB {
+	return &CourseDB{
+		dbConn:  dbConn,
+		Queries: db.New(dbConn),
+	}
+}
+
+func (c *CourseDB) callTx(ctx context.Context, fn func(*db.Queries) error) error {
+	tx, err := c.dbConn.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	q := db.New(tx)
+	err = fn(q)
+	if err != nil {
+		if errRb := tx.Rollback(); errRb != nil {
+			return fmt.Errorf(`Error on rollback: %v, original error: %v`, errRb, err)
+		}
+		return err
+	}
+	return tx.Commit()
+}
+
+func main() {
+	ctx := context.Background()
+	dbConn, err := sql.Open("mysql", "root:@tcp(localhost:3306)/go_courses")
+	if err != nil {
+		panic(err)
+	}
+	defer dbConn.Close()
+
+	queries := db.New(dbConn)
+
+}
